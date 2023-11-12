76828692a442dc9073ce7af54bd698920214de05
dinkloc <nguyendinhloc502@gmail.com>
2023-11-12 11:23:13 +0700

    a few ui tweaks

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -1,41 +1,58 @@
-import { useState, useEffect } from "react";
 import Router from "next/router";
 import useRequest from "../../hooks/use-request";
-import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+import styled from "styled-components";
+import { useState } from "react";
 
-// TODO remove, this demo shouldn't need to reset the theme.
+const H1 = styled.h1`
+  text-align: center;
+`;
 
-const defaultTheme = createTheme();
+const FormGroup = styled.div`
+  margin-bottom: 1.5rem;
+  text-align: center;
+`;
+
+const LabelEmail = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  font-weight: 500;
+  color: #111827;
+  text-align: right;
+  width: 29%;
+`;
+
+const LabelPassword = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  font-weight: 500;
+  color: #111827;
+  text-align: right;
+  width: 31%;
+`;
+
+const Input = styled.input`
+  display: inline-block;
+  padding: 0.7rem;
+  width: 300px;
+  border-radius: 0.5rem;
+  border-width: 1px;
+  border-color: #d1d5db;
+  width: 50%;
+  color: #111827;
+  background-color: #f9fafb;
+`;
+
+const DivButton = styled.div`
+  display: block;
+  text-align: right;
+  display: block;
+  width: 75%;
+`;
+const Button = styled.button``;
 
 export default () => {
   const [email, setEmail] = useState("");
@@ -57,83 +74,32 @@ export default () => {
   };
 
   return (
-    <>
+    <div>
       <form onSubmit={onSubmit}>
-        <h1>Sign In</h1>
-        <div className="form-group">
-          <label>Email Address</label>
-          <input
+        <H1>Sign In</H1>
+        <FormGroup className="form-group">
+          <LabelEmail>Email</LabelEmail>
+          <Input
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             className="form-control"
           />
-        </div>
-        <div className="form-group">
-          <label>Password</label>
-          <input
+        </FormGroup>
+        <FormGroup className="form-group">
+          <LabelPassword>Password</LabelPassword>
+          <Input
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             type="password"
             className="form-control"
           />
-        </div>
+        </FormGroup>
         {errors}
-        <button className="btn btn-primary">Sign In</button>
+
+        <DivButton>
+          <Button className="btn btn-primary">Sign In</Button>
+        </DivButton>
       </form>
-      {/* <ThemeProvider theme={defaultTheme}>
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Box component="form" noValidate onSubmit={onSubmit} sx={{ mt: 3 }}>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="new-password"
-                  />
-                </Grid>
-              </Grid>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-            </Box>
-          </Box>
-          <Copyright sx={{ mt: 5 }} />
-        </Container>
-      </ThemeProvider> */}
-    </>
+    </div>
   );
 };
--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -8,12 +8,13 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
-    const { id, title, price } = data;
+    const { id, title, price, userId } = data;
 
     const ticket = Ticket.build({
       id,
       title,
       price,
+      userId,
     });
     await ticket.save();
 
