d88cbe2a8999697d60f4698b755ee3a82677ad3d
dinkloc <nguyendinhloc502@gmail.com>
2023-11-25 15:54:32 +0700

    testing deployment

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,11 @@ import { app } from "./app";
 const start = async () => {
   console.log("stating up..... ");
   console.log("stating up..... ");
-
+  console.log("stating up..... ");
+  console.log("stating up..... ");
+  console.log("stating up..... ");
+  console.log("stating up..... ");
+  console.log("stating up..... ");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
