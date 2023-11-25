38c82dbd55b3fb57f17ad9c2fb6b633a1fa1159f
dinkloc <nguyendinhloc502@gmail.com>
2023-11-25 11:08:25 +0700

    test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import { app } from "./app";
 
 const start = async () => {
   console.log("stating up..... ");
+  console.log("stating up..... ");
 
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
